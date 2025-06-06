748c0fba221d0ca623ff3d202b3bb8ec30be4365
jesus jimenez martinez <jimenezmartinezjesus76@gmail.com>
2025-06-06 15:11:18 -0500

    Se agrega al proyecto

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+
+  output: 'static', // Netlify funciona mejor con sitios estáticos (SSG)
+  site: 'https://rainbow-maamoul-a6a84f.netlify.app', // Reemplaza con tu URL de Netlify
+
+});
+
+
